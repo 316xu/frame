2c9b51906910d7e2caca1080f92664648ef59858
jfxu <hustjifa@gmail.com>
2019-09-30 19:46:24 +0800

    tmp

--- wxmini/src/main.js
+++ wxmini/src/main.js
@@ -6,6 +6,8 @@ var Fly = require('flyio/dist/npm/wx')
 var fly = new Fly()
 fly.config.baseURL = process.env.BASE_URL
 
+Vue.prototype.$http=fly
+
 Vue.config.productionTip = false
 App.mpType = 'app'
 
--- wxmini/src/pages/products/products.vue
+++ wxmini/src/pages/products/products.vue
@@ -3,15 +3,19 @@
 
   <view class="section">
     <view class="product-list" style="flex-direction:row;">
-      <view class="flex-item bc_green">
-
-        <scroll-view  style="{'height': '100%'}" :scroll-y="true">
+      <view class="flex-item">
+        <scroll-view style="height: 100vh;" :scroll-y="true">
           <div :class="['type-item', {choosen:choosenType==type.id,chooseno:choosenType!=type.id}]" v-for="(type, index) in types" :key="index" @click="chooseItem(type.id)"> {{ type.name }} </div>
+          <!-- <div :class="['type-item']" v-for="item in goodMap" :key="item.id"> {{ item.name }} </div> -->
         </scroll-view>
-
-
       </view>
-      <view class="flex-item bc_blue">3</view>
+      <view class="flex-item">
+        <scroll-view style="height: 100vh;" :scroll-y="true" :scroll-into-view="'id' + toGood">
+          <div :id="'id' + good.id" :class="['good-item']" v-for="(good, key) in goods" :key="key">
+             {{ good.name }} 
+          </div>
+        </scroll-view>
+      </view>
     </view>
   </view>
 
@@ -36,52 +40,38 @@ export default {
         {
           name: '新品',
           id: 1,
-        }, {
-          name: '热销',
-          id: 2,
-        }, {
-          name: '炒菜',
-          id: 3,
-        },{
-          name: '炒菜',
-          id: 3,
-        },{
-          name: '炒菜',
-          id: 3,
-        },{
-          name: '炒菜',
-          id: 3,
-        },{
-          name: '炒菜',
-          id: 3,
-        },{
-          name: '炒菜',
-          id: 3,
-        },{
-          name: '炒菜',
-          id: 3,
-        },
+        }
       ],
-      choosenType: 2,
-      imgUrls: [
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/newsPicture/05558951-de60-49fb-b674-dd906c8897a6',
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/coursePicture/0fbcfdf7-0040-4692-8f84-78bb21f3395d',
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/management-school-picture/7683b32e-4e44-4b2f-9c03-c21f34320870'
-      ],
-      code: '',
-      canIUse: wx.canIUse('button.open-type.getUserInfo')
+      goodMap: {},
+      goods: [],
+      choosenType: 0,
+      toGood: '',
+
     }
   },
 
   created () {
-
+    this.$http.get('good/list', {
+      shopId: 1
+    }).then(({data}) => {
+      this.types=data.data.types
+      this.goodMap = data.data.goods
+      for (let item in this.goodMap) {
+        this.goods = this.goods.concat(this.goodMap[item])
+      }
+      this.choosenType = this.types[0].id
+    })
 
   },
 
   methods: {
-    chooseItem(id) {
-      this.choosenType = id
-      console.log(this.choosenType)
+    chooseItem(typeId) {
+      this.choosenType = typeId
+      for (let item in this.goodMap) {
+        if (this.goodMap[item][0].typeId == typeId) {
+          this.toGood = this.goodMap[item][0].id
+        }
+      }
     }
   }
 }
@@ -97,11 +87,23 @@ export default {
   display: flex;
   height: 100%;
 }
+
+.flex-item {
+  height: 100%;
+  overflow-y: auto;
+}
+
+
+
 .type_list {background: green;width:20%; height: 100px;}
 
 .type-item {
-  width: 100rpx;
+  width: 150rpx;
   text-align: center;
+}
+
+.good-item {
+  height: 80rpx;
 }
 
 .type-item.chooseno {
